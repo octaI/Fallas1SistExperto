b752c7c6f2a4a21cff8620b0a962caf309c0175f
Octavio <octaviomdq93@gmail.com>
2018-07-17 14:00:53 -0300

    Finished all indicators Started main menu
    TODO: Add Rules

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ Estructura del archivo de entrada
 Monto Negociado|N° Oper
 
 """
-
+import numpy as np
 import pandas as pd
 import statistics
-import sklearn
-
+from sklearn import linear_model
 
 def load_database():
     excel_file = pd.ExcelFile("Merarg-input.xls")
@@ -41,44 +40,81 @@ def calculate_ema(number, price_list):
 def calculate_MACD(price_list):
     return calculate_ema(12, price_list) - calculate_ema(26, price_list)
 
-def calculate_PPO (price_list):
-    return calculate_MACD(price_list) / calculate_ema(26,price_list) * 100
 
-def calculate_RSI (price_list):
-    relevant_numbers = price_list[-14:] #taking the last 14 days
+def calculate_PPO(price_list):
+    return (calculate_MACD(price_list) / calculate_ema(26, price_list)) * 100
+
+
+def calculate_RSI(price_list):
+    relevant_numbers = price_list[-14:]  # taking the last 14 days
     avg_gain = 0
     avg_loss = 0
     for num in relevant_numbers:
-        if (num < 0): avg_gain+=num
+        if num < 0:
+            avg_gain += num
 
         else:
-            avg_loss+=(-num)
-    avg_gain = avg_gain/14
-    avg_loss = avg_loss/14
-    relative_strength = avg_gain/avg_loss
-    return (100 - (100/(1+relative_strength)))
+            avg_loss += (-num)
+    avg_gain = avg_gain / 14
+    avg_loss = avg_loss / 14
+    relative_strength = avg_gain / avg_loss
+    return 100 - (100 / (1 + relative_strength))
+
 
 def calculate_error_bands(price_list):
-    regr = sklearn.linear_model.LinearRegression()
-    x_values = list(range(1,31)) #taking the last 30 days for linear regression
-    relevant_prices = price_list[-30]
-    y_values = []
-    for elem in relevant_prices:
-        y_values.append(elem(0))
-    regr.fit(x_values,y_values)
-    std_dev = statistics.pstdev(y_values)
-    return regr.predict(30) + 1.96*std_dev, regr.predict(30 - 1.96*std_dev)
+    regr = linear_model.LinearRegression()
+    x_values = np.array(list(range(1, len(price_list)+1)))  # taking the last 30 days for linear regression
+    x_values = x_values.reshape(-1, 1)
+    y_values = np.array(price_list)
+    y_values = y_values.reshape(-1,1)
+    regr.fit(x_values, y_values)
+    std_dev = statistics.pstdev(price_list) #std deviation of the price list
+    desired_point = np.array([len(price_list)]).reshape(-1,1)
+    return [regr.predict(desired_point)[0,0] + 1.96*std_dev,regr.predict(desired_point)[0,0]-1.96*std_dev]
 
-def main():
+def initial_load():
     actions_sheet = load_database()
     relevant_data = actions_sheet[['Especie', 'Cierre del día', 'Variación %']]
     actions_dictionary = {}
     for index, row in relevant_data.iterrows():
         if row[0] not in actions_dictionary:
-            actions_dictionary[row[0]] = [(row[1],row[2])]
+            actions_dictionary[row[0]] = [(row[1], row[2])]
         else:
-            actions_dictionary[row[0]].append((row[1],row[2]))
-    print(actions_dictionary['AGRO'])
+            actions_dictionary[row[0]].append((row[1], row[2]))
+
+    return actions_dictionary
+
+
+def analyze_indicators(price_list):
+
+    return
+
+def main():
+    actions_dictionary = initial_load()
+    print("*************************************")
+    print("BIENVENIDO AL ANALIZADOR DE ACCIONES")
+    print("*************************************")
+    print("Por favor ingrese la Especie que desea analizar")
+    print("Usted tiene a disposicion las siguientes especies:")
+    names_df =pd.DataFrame({'Especie': list(actions_dictionary.keys())})
+    with pd.option_context('display.max_rows',None,'display.max_columns',names_df.shape[1],'expand_frame_repr',True):
+        print(names_df)
+
+    valid_name = False
+    while (not valid_name):
+        action_name = input("Accion: ")
+        action_name = action_name.upper()
+        if action_name.upper() not in actions_dictionary.keys():
+            print("Nombre de especie incorrecto. Por favor ingrese un nombre válido")
+        else:
+            print("Usted ha elegido "+action_name)
+            valid_name = True
+    action_data = actions_dictionary[action_name] #getting the requested info
+    variations_list = []
+    prices_list = []
+    for elem in action_data:
+        variations_list.append(elem[1])
+        prices_list.append(elem[0])  #splitting into the relevant lists
 
 if __name__ == "__main__":
     main()
